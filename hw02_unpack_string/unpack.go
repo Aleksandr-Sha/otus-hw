a6cd29ad3a28abd597de93ea5cd79d4c6a38bc1e
Aleksandr Shaparenko <sanek.7144241@gmail.com>
2026-02-25 20:49:33 +0300

    hw2: add solution task (#2)
    * hw2: update unpack func
    
    * hw2: update unpacker
    
    * hw2: delete .sync
    
    * hw2: refactor
    
    * hw2: refactor
    
    * hw2: refactor
    
    * hw2: refactor
    
    * hw2: refactor and add tests
    
    * hw2: refactor
    
    * hw2: refactor
    
    * hw2: refactor
    
    * hw2: refactor
    
    * hw2: refactor
    
    * hw2: refactor

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,167 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+const (
+	Digit = iota
+	Other
+	Empty
+)
+
+const BackSlashCode = 92
+
+type symbol struct {
+	val        rune
+	symbolType int
+}
+
+func (s *symbol) isDigit() bool {
+	return s.symbolType == Digit
+}
+
+func (s *symbol) isEmpty() bool {
+	return s.symbolType == Empty
+}
+
+type unpacker struct {
+	currentIndex   int
+	runs           []rune
+	builder        strings.Builder
+	symbolForWrite *symbol
+}
+
+func newUnpacker(str string) *unpacker {
+	return &unpacker{
+		currentIndex: 0,
+		runs:         []rune(str),
+	}
+}
+
+func (u *unpacker) unpackString() (string, error) {
+	err := u.updateSymbolForWrite()
+	if err != nil {
+		return "", fmt.Errorf("read first symbol: %w", err)
+	}
+
+	for u.isNotAllRead() {
+		if u.symbolForWrite.isDigit() {
+			return "", fmt.Errorf(
+				"first symbol before write iteration digit %s: %w",
+				string(u.symbolForWrite.val),
+				ErrInvalidString,
+			)
+		}
+
+		if u.isFinishRead() {
+			u.writeCurrentSymbolAndSetNew(&symbol{symbolType: Empty})
+			continue
+		}
+
+		nextSymbol, err := u.getNextSymbol()
+		if err != nil {
+			return "", fmt.Errorf("read next symbol: %w", err)
+		}
+
+		if nextSymbol.isDigit() {
+			err := u.repeatAndWriteCurrentSymbol(nextSymbol)
+			if err != nil {
+				return "", fmt.Errorf("repeat and write: %w", err)
+			}
+
+			err = u.updateSymbolForWrite()
+			if err != nil {
+				return "", fmt.Errorf("after repeat and write: %w", err)
+			}
+		} else {
+			u.writeCurrentSymbolAndSetNew(nextSymbol)
+		}
+	}
+
+	return u.builder.String(), nil
+}
+
+func (u *unpacker) updateSymbolForWrite() error {
+	newSymbolForWrite, err := u.getNextSymbol()
+	if err != nil {
+		return fmt.Errorf("update symbol for write: %w", err)
+	}
+
+	u.symbolForWrite = newSymbolForWrite
+	return nil
+}
+
+func (u *unpacker) writeCurrentSymbolAndSetNew(newSymbol *symbol) {
+	u.builder.WriteRune(u.symbolForWrite.val)
+	u.symbolForWrite = newSymbol
+}
+
+func (u *unpacker) repeatAndWriteCurrentSymbol(countSymbol *symbol) error {
+	countRepeat, err := strconv.Atoi(string(countSymbol.val))
+	if err != nil {
+		return fmt.Errorf("parse digit: %w", err)
+	}
+
+	repeatPart := strings.Repeat(string(u.symbolForWrite.val), countRepeat)
+
+	u.builder.WriteString(repeatPart)
+
+	return nil
+}
+
+func (u *unpacker) getNextSymbol() (*symbol, error) {
+	if u.isFinishRead() {
+		return &symbol{symbolType: Empty}, nil
+	}
+
+	newRune := u.runs[u.currentIndex]
+	u.currentIndex++
+
+	if unicode.IsDigit(newRune) {
+		return &symbol{val: newRune, symbolType: Digit}, nil
+	}
+
+	if newRune == BackSlashCode {
+		if !u.isFinishRead() {
+			symbolAfterBackSlashCode := u.runs[u.currentIndex]
+			u.currentIndex++
+
+			if unicode.IsDigit(symbolAfterBackSlashCode) || symbolAfterBackSlashCode == BackSlashCode {
+				return &symbol{val: symbolAfterBackSlashCode, symbolType: Other}, nil
+			}
+		}
+
+		return nil, fmt.Errorf("get escaped symbol: %w", ErrInvalidString)
+	}
+
+	return &symbol{val: newRune, symbolType: Other}, nil
+}
+
+func (u *unpacker) isFinishRead() bool {
+	return u.currentIndex == len(u.runs)
+}
+
+func (u *unpacker) isNotAllRead() bool {
+	return !u.symbolForWrite.isEmpty()
+}
+
+func Unpack(str string) (string, error) {
+	if str == "" {
+		return "", nil
+	}
+
+	unpacker := newUnpacker(str)
+
+	unpackString, err := unpacker.unpackString()
+	if err != nil {
+		return "", fmt.Errorf("unpack: %w", err)
+	}
+
+	return unpackString, nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -17,16 +17,18 @@ func TestUnpack(t *testing.T) {
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
 		{input: "🙃0", expected: ""},
+		{input: "✈2🚀3🌏🚢5🌴🌴2🏫0🚚8", expected: "✈✈🚀🚀🚀🌏🚢🚢🚢🚢🚢🌴🌴🌴🚚🚚🚚🚚🚚🚚🚚🚚"},
 		{input: "aaф0b", expected: "aab"},
-		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: "月3人水山0", expected: "月月月人水"},
+		{input: "안5ABS", expected: "안안안안안ABS"},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\\\3人6`, expected: `qwe\3人人人人人人`},
 	}
 
 	for _, tc := range tests {
-		tc := tc
 		t.Run(tc.input, func(t *testing.T) {
 			result, err := Unpack(tc.input)
 			require.NoError(t, err)
@@ -36,9 +38,8 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", `qw\ne`}
 	for _, tc := range invalidStrings {
-		tc := tc
 		t.Run(tc, func(t *testing.T) {
 			_, err := Unpack(tc)
 			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)

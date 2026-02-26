3fd9a8b95171a1abb4e68bf623ed1fc541fbde03
Aleksandr Shaparenko <sanek.7144241@gmail.com>
2026-02-26 20:04:34 +0300

    Hw03 frequency analysis (#3)
    * hw3: add solution task
    
    * hw3: refactoring
    
    * hw3: refactoring
    
    * hw3: tidy
    
    * hw3: refactor
    
    * hw3: refactor
    
    * hw3: refactor

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,78 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+	"strings"
+)
+
+var (
+	punctuationMarksFromEdgesRegexp = regexp.MustCompile(`^\p{P}+|\p{P}+$`)
+	dashesRegexp                    = regexp.MustCompile(`^-+$`)
+)
+
+type wordCounter struct {
+	text          string
+	wordsCountMap map[string]int
+	wordsResult   []string
+}
+
+func newWordCounter(text string) *wordCounter {
+	return &wordCounter{text: text, wordsCountMap: make(map[string]int), wordsResult: make([]string, 0)}
+}
+
+func (w *wordCounter) getTop10Word() []string {
+	for _, word := range strings.Fields(w.text) {
+		if word != "-" {
+			w.processAndCountWord(word)
+		}
+	}
+
+	w.sortWords()
+
+	return w.getTop10FromResult()
+}
+
+func (w *wordCounter) processAndCountWord(word string) {
+	if !dashesRegexp.MatchString(word) {
+		word = punctuationMarksFromEdgesRegexp.ReplaceAllString(strings.ToLower(word), "")
+	}
+
+	if _, ok := w.wordsCountMap[word]; ok {
+		w.wordsCountMap[word]++
+	} else {
+		w.wordsCountMap[word] = 1
+		w.wordsResult = append(w.wordsResult, word)
+	}
+}
+
+func (w *wordCounter) sortWords() {
+	sort.Slice(w.wordsResult, func(i, j int) bool {
+		iCount := w.wordsCountMap[w.wordsResult[i]]
+		jCount := w.wordsCountMap[w.wordsResult[j]]
+
+		if iCount == jCount {
+			return strings.Compare(w.wordsResult[i], w.wordsResult[j]) < 0
+		}
+
+		return iCount > jCount
+	})
+}
+
+func (w *wordCounter) getTop10FromResult() []string {
+	if len(w.wordsResult) < 10 {
+		return w.wordsResult[:len(w.wordsResult):len(w.wordsResult)]
+	}
+
+	return w.wordsResult[:10:10]
+}
+
+func Top10(text string) []string {
+	if text == "" {
+		return []string{}
+	}
+
+	wordCounter := newWordCounter(text)
+
+	return wordCounter.getTop10Word()
 }
--- hw03_frequency_analysis/top_test.go
+++ hw03_frequency_analysis/top_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 // Change to true if needed.
-var taskWithAsteriskIsCompleted = false
+var taskWithAsteriskIsCompleted = true
 
 var text = `Как видите, он  спускается  по  лестнице  вслед  за  своим
 	другом   Кристофером   Робином,   головой   вниз,  пересчитывая
@@ -79,4 +79,55 @@ func TestTop10(t *testing.T) {
 			require.Equal(t, expected, Top10(text))
 		}
 	})
+}
+
+func TestTop10WhenResultLenLess10(t *testing.T) {
+	text := "Генри Форд - владелец заводов по производству автомобилей по всему миру"
+
+	expected := []string{
+		"по",           // 2
+		"автомобилей",  // 1
+		"владелец",     // 1
+		"всему",        // 1
+		"генри",        // 1
+		"заводов",      // 1
+		"миру",         // 1
+		"производству", // 1
+		"форд",         // 1
+	}
+
+	result := Top10(text)
+	require.Equal(t, expected, result)
+	require.Len(t, result, len(expected))
+}
+
+func TestTop10WhenWordsHaveDifferentCaseAndPunctuationMarks(t *testing.T) {
+	text := "Слово -Слово; сЛово!"
+
+	expected := []string{"слово"}
+
+	require.Equal(t, expected, Top10(text))
+}
+
+func TestTop10WhenWordsHaveDifferentPunctuationMarksInMiddle(t *testing.T) {
+	text := "Слово сло-во сло..во"
+
+	expected := []string{
+		"сло-во",  // 1
+		"сло..во", // 1
+		"слово",   // 1
+	}
+
+	require.Equal(t, expected, Top10(text))
+}
+
+func TestTop10WhenWordsFromDashes(t *testing.T) {
+	text := "- - --- -------"
+
+	expected := []string{
+		"---",     // 1
+		"-------", // 1
+	}
+
+	require.Equal(t, expected, Top10(text))
 }

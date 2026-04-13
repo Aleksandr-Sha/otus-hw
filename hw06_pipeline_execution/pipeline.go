9c9a225eec7370ba48ede344fd38930864e3040c
Aleksandr Shaparenko <sanek.7144241@gmail.com>
2026-04-13 18:18:09 +0300

    Hw06 pipeline execution (#6)
    * hw6: add solution
    
    * hw6: refactoring
    
    * hw6: refactoring and add test
    
    * hw6: refactoring and add test
    
    * hw6: delete sync
    
    * hw6: refactoring
    
    * hw6: refactoring
    
    * hw6: refactoring
    
    * hw6: refactoring and tidy

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,73 @@ type (
 
 type Stage func(in In) (out Out)
 
+const CurrentStageIndex = 0
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	if len(stages) == 0 {
+		return runWithoutStages(in, done)
+	}
+
+	return runStages(in, done, stages)
+}
+
+func runStages(in In, done In, stage []Stage) Out {
+	inProxy := make(Bi)
+
+	go readInputDataAndProxy(in, done, inProxy)
+
+	out := stage[CurrentStageIndex](inProxy)
+
+	if len(stage) > 1 {
+		return runStages(out, done, stage[1:])
+	}
+
+	return out
+}
+
+func runWithoutStages(in In, done In) Out {
+	out := make(Bi)
+	go readInputDataAndProxy(in, done, out)
+	return out
+}
+
+func readInputDataAndProxy(in In, done In, inProxy Bi) {
+	func() {
+		defer func() {
+			close(inProxy)
+			// Ждём пока закроется канал вывода информации с предыдущего stage (в рамках stage канал out) или первичный
+			// канал получения данных если stage первый, обеспечив тем самым гарантированный выход из for range внутри
+			// stage или отсутствие блокировок первичного писателя соответственно (если какие-то данные перед закрытием
+			// успели встать на запись)
+			waitCloseChan(in)
+		}()
+		for {
+			select {
+			case <-done:
+				return
+			default:
+				select {
+				case <-done:
+					return
+				case v, ok := <-in:
+
+					if !ok {
+						return
+					}
+
+					inProxy <- v
+				}
+			}
+		}
+	}()
+}
+
+func waitCloseChan(in In) {
+	for {
+		_, ok := <-in
+
+		if !ok {
+			return
+		}
+	}
 }
--- hw06_pipeline_execution/pipeline_test.go
+++ hw06_pipeline_execution/pipeline_test.go
@@ -15,20 +15,7 @@ const (
 )
 
 func TestPipeline(t *testing.T) {
-	// Stage generator
-	g := func(_ string, f func(v interface{}) interface{}) Stage {
-		return func(in In) Out {
-			out := make(Bi)
-			go func() {
-				defer close(out)
-				for v := range in {
-					time.Sleep(sleepPerStage)
-					out <- f(v)
-				}
-			}()
-			return out
-		}
-	}
+	g := createStageGeneratorWithoutWg()
 
 	stages := []Stage{
 		g("Dummy", func(v interface{}) interface{} { return v }),
@@ -41,12 +28,7 @@ func TestPipeline(t *testing.T) {
 		in := make(Bi)
 		data := []int{1, 2, 3, 4, 5}
 
-		go func() {
-			for _, v := range data {
-				in <- v
-			}
-			close(in)
-		}()
+		go writeInputData(data, in)
 
 		result := make([]string, 0, 10)
 		start := time.Now()
@@ -67,19 +49,9 @@ func TestPipeline(t *testing.T) {
 		done := make(Bi)
 		data := []int{1, 2, 3, 4, 5}
 
-		// Abort after 200ms
 		abortDur := sleepPerStage * 2
-		go func() {
-			<-time.After(abortDur)
-			close(done)
-		}()
-
-		go func() {
-			for _, v := range data {
-				in <- v
-			}
-			close(in)
-		}()
+		go closeDoneChanAfterTimeout(abortDur, done)
+		go writeInputData(data, in)
 
 		result := make([]string, 0, 10)
 		start := time.Now()
@@ -93,10 +65,130 @@ func TestPipeline(t *testing.T) {
 	})
 }
 
-func TestAllStageStop(t *testing.T) {
+func TestAllStageStopWhenDone(t *testing.T) {
 	wg := sync.WaitGroup{}
-	// Stage generator
-	g := func(_ string, f func(v interface{}) interface{}) Stage {
+	g := createStageGeneratorWithWg(&wg)
+
+	stages := []Stage{
+		g("Dummy", func(v interface{}) interface{} { return v }),
+		g("Multiplier (* 2)", func(v interface{}) interface{} { return v.(int) * 2 }),
+		g("Adder (+ 100)", func(v interface{}) interface{} { return v.(int) + 100 }),
+		g("Stringifier", func(v interface{}) interface{} { return strconv.Itoa(v.(int)) }),
+	}
+
+	t.Run("done case", func(t *testing.T) {
+		in := make(Bi)
+		done := make(Bi)
+		data := []int{1, 2, 3, 4, 5}
+
+		go closeDoneChanAfterTimeout(sleepPerStage*2, done)
+		go writeInputData(data, in)
+
+		result := make([]string, 0, 10)
+		for s := range ExecutePipeline(in, done, stages...) {
+			result = append(result, s.(string))
+		}
+		wg.Wait()
+
+		require.Len(t, result, 0)
+	})
+}
+
+func TestAllStageStopWhenProcessAllData(t *testing.T) {
+	wg := sync.WaitGroup{}
+	g := createStageGeneratorWithWg(&wg)
+
+	stages := []Stage{
+		g("Dummy", func(v interface{}) interface{} { return v }),
+		g("Multiplier (* 2)", func(v interface{}) interface{} { return v.(int) * 2 }),
+		g("Adder (+ 100)", func(v interface{}) interface{} { return v.(int) + 100 }),
+		g("Stringifier", func(v interface{}) interface{} { return strconv.Itoa(v.(int)) }),
+	}
+
+	in := make(Bi)
+	data := []int{1, 2, 3, 4, 5}
+
+	go writeInputData(data, in)
+
+	result := make([]string, 0, 10)
+	for s := range ExecutePipeline(in, nil, stages...) {
+		result = append(result, s.(string))
+	}
+
+	wg.Wait()
+
+	require.Equal(t, []string{"102", "104", "106", "108", "110"}, result)
+}
+
+func TestOneStageWhenProcessAllData(t *testing.T) {
+	wg := sync.WaitGroup{}
+	g := createStageGeneratorWithWg(&wg)
+
+	stages := []Stage{g("Multiplier (* 2)", func(v interface{}) interface{} { return v.(int) * 2 })}
+
+	in := make(Bi)
+	data := []int{1, 2, 3, 4, 5}
+
+	go writeInputData(data, in)
+
+	result := make([]int, 0, 10)
+	for s := range ExecutePipeline(in, nil, stages...) {
+		result = append(result, s.(int))
+	}
+
+	wg.Wait()
+
+	require.Equal(t, []int{2, 4, 6, 8, 10}, result)
+}
+
+func TestZeroStageWhenProcessAllData(t *testing.T) {
+	in := make(Bi)
+	data := []int{1, 2, 3, 4, 5}
+
+	go writeInputData(data, in)
+
+	result := make([]int, 0, 10)
+	for s := range ExecutePipeline(in, nil, []Stage{}...) {
+		result = append(result, s.(int))
+	}
+
+	require.Equal(t, []int{1, 2, 3, 4, 5}, result)
+}
+
+func TestZeroStageWhenDone(t *testing.T) {
+	in := make(Bi)
+	done := make(Bi)
+	data := []int{1, 2, 3, 4, 5}
+
+	go close(done)
+	go writeInputData(data, in)
+
+	result := make([]int, 0, 10)
+	for s := range ExecutePipeline(in, done, []Stage{}...) {
+		result = append(result, s.(int))
+	}
+
+	require.Len(t, result, 0)
+}
+
+func createStageGeneratorWithoutWg() func(_ string, f func(v interface{}) interface{}) Stage {
+	return func(_ string, f func(v interface{}) interface{}) Stage {
+		return func(in In) Out {
+			out := make(Bi)
+			go func() {
+				defer close(out)
+				for v := range in {
+					time.Sleep(sleepPerStage)
+					out <- f(v)
+				}
+			}()
+			return out
+		}
+	}
+}
+
+func createStageGeneratorWithWg(wg *sync.WaitGroup) func(_ string, f func(v interface{}) interface{}) Stage {
+	return func(_ string, f func(v interface{}) interface{}) Stage {
 		return func(in In) Out {
 			out := make(Bi)
 			wg.Add(1)
@@ -111,40 +203,20 @@ func TestAllStageStop(t *testing.T) {
 			return out
 		}
 	}
+}
 
-	stages := []Stage{
-		g("Dummy", func(v interface{}) interface{} { return v }),
-		g("Multiplier (* 2)", func(v interface{}) interface{} { return v.(int) * 2 }),
-		g("Adder (+ 100)", func(v interface{}) interface{} { return v.(int) + 100 }),
-		g("Stringifier", func(v interface{}) interface{} { return strconv.Itoa(v.(int)) }),
-	}
-
-	t.Run("done case", func(t *testing.T) {
-		in := make(Bi)
-		done := make(Bi)
-		data := []int{1, 2, 3, 4, 5}
-
-		// Abort after 200ms
-		abortDur := sleepPerStage * 2
-		go func() {
-			<-time.After(abortDur)
-			close(done)
-		}()
-
-		go func() {
-			for _, v := range data {
-				in <- v
-			}
-			close(in)
-		}()
-
-		result := make([]string, 0, 10)
-		for s := range ExecutePipeline(in, done, stages...) {
-			result = append(result, s.(string))
+func writeInputData(data []int, in Bi) {
+	func() {
+		for _, v := range data {
+			in <- v
 		}
-		wg.Wait()
+		close(in)
+	}()
+}
 
-		require.Len(t, result, 0)
-
-	})
+func closeDoneChanAfterTimeout(abortDur time.Duration, done Bi) {
+	func() {
+		<-time.After(abortDur)
+		close(done)
+	}()
 }

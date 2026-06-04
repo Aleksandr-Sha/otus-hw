d8b09ab16fce712078b6c8afe52cd234c0f650ba
ShaparenkoAA <ShaparenkoAA@bcs.ru>
2026-06-04 19:02:28 +0300

    hw7: add test

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -13,9 +13,7 @@ import (
 
 const TmpFileNamePattern = "tmp_for_copy"
 
-var (
-	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
-)
+var ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
 	fileForCopyFrom, stat, err := preparingFileForCopyFrom(fromPath, offset)
@@ -30,7 +28,7 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 	}
 	defer closeFileWithErrorHandle(tempFileForCopyTo)
 
-	bar, proxyReader := getReaderWithProgressBar(fileForCopyFrom, stat, offset, limit)
+	bar, proxyReader := getReaderProxyWithProgressBar(fileForCopyFrom, stat, offset, limit)
 	defer bar.Finish()
 
 	err = copyData(proxyReader, tempFileForCopyTo, limit)
@@ -39,48 +37,6 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 	}
 
 	return nil
-}
-
-func closeFileWithErrorHandle(fileForCopyFrom *os.File) {
-	err := fileForCopyFrom.Close()
-	if err != nil {
-		log.Printf("close file for copy from: %s", err.Error())
-	}
-}
-
-func copyData(reader io.Reader, writer io.Writer, limit int64) error {
-	if limit == 0 {
-		_, err := io.Copy(writer, reader)
-		if err != nil {
-			return fmt.Errorf("full copy : %w", err)
-		}
-	} else {
-		_, err := io.CopyN(writer, reader, limit)
-		if err != nil {
-			return fmt.Errorf("limit copy : %w", err)
-		}
-	}
-
-	return nil
-}
-
-func getReaderWithProgressBar(fileForCopyFrom *os.File, stat os.FileInfo, offset, limit int64) (*pb.ProgressBar, io.Reader) {
-	bar := pb.Full.Start64(getBarSize(stat, offset, limit))
-	return bar, bar.NewProxyReader(fileForCopyFrom)
-}
-
-func preparingTempFileForCopyTo(toPath string) (*os.File, error) {
-	absPath, err := filepath.Abs(toPath)
-	if err != nil {
-		return nil, fmt.Errorf("get file path: %w", err)
-	}
-
-	file, err := os.CreateTemp(absPath, TmpFileNamePattern)
-	if err != nil {
-		return nil, fmt.Errorf("create file: %w", err)
-	}
-
-	return file, nil
 }
 
 func preparingFileForCopyFrom(fromPath string, offset int64) (*os.File, os.FileInfo, error) {
@@ -113,6 +69,48 @@ func preparingFileForCopyFrom(fromPath string, offset int64) (*os.File, os.FileI
 	}
 
 	return file, fileInfo, nil
+}
+
+func preparingTempFileForCopyTo(toPath string) (*os.File, error) {
+	absPath, err := filepath.Abs(toPath)
+	if err != nil {
+		return nil, fmt.Errorf("get file path: %w", err)
+	}
+
+	file, err := os.CreateTemp(absPath, TmpFileNamePattern)
+	if err != nil {
+		return nil, fmt.Errorf("create file: %w", err)
+	}
+
+	return file, nil
+}
+
+func getReaderProxyWithProgressBar(fileForCopyFrom *os.File, stat os.FileInfo, offset, limit int64) (*pb.ProgressBar, io.Reader) {
+	bar := pb.Full.Start64(getBarSize(stat, offset, limit))
+	return bar, bar.NewProxyReader(fileForCopyFrom)
+}
+
+func copyData(reader io.Reader, writer io.Writer, limit int64) error {
+	if limit == 0 {
+		_, err := io.Copy(writer, reader)
+		if err != nil {
+			return fmt.Errorf("full copy : %w", err)
+		}
+	} else {
+		_, err := io.CopyN(writer, reader, limit)
+		if err != nil {
+			return fmt.Errorf("limit copy : %w", err)
+		}
+	}
+
+	return nil
+}
+
+func closeFileWithErrorHandle(file *os.File) {
+	err := file.Close()
+	if err != nil {
+		log.Printf("close file for copy from: %s", err.Error())
+	}
 }
 
 func getBarSize(stat os.FileInfo, offset, limit int64) int64 {
